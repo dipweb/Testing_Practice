dabb82b5c8f6e49743ffd18b8d4e6252708a4399
kumarde <kumarde@cisco.com>
2023-12-18 09:39:31 +0530

    starting event testing

--- src/App.js
+++ src/App.js
@@ -2,18 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <p>First Testing App </p>
-
-      <input type="text" placeholder="Enter User Name" name="userName" />
-      <br />
-      <img
-        title="Book Image"
-        src="https://www.wakeupbooks.in/wp-content/uploads/2022/11/My-Book-of-facts.png"
-      />
-    </div>
-  );
+  return <div>My APP</div>;
 }
 
 export default App;
